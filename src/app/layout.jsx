9b2d64c6de7bc2864a7fc4add754d27347a52d53
Alexandre Parent <alexp90300@gmail.com>
2025-02-05 16:53:37 +0100

    ajout favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,6 +25,18 @@ export const metadata = {
   openGraph: {
     images: '/og-image.png',
   },
+  icons: {
+    icon: [
+      { url: '/img/icons/favicon.ico' },
+      { url: '/img/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/img/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/img/icons/apple-touch-icon.png' }
+    ]
+  }
 }
 import '@fontsource/dancing-script';
 
@@ -35,9 +47,17 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} ${firaCode.variable} scroll-smooth`}
       suppressHydrationWarning
     >
+      <head>
+        <link rel="icon" href="/img/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/img/icons/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/img/icons/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" href="/img/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/img/icons/site.webmanifest" />
+      </head>
       <body className="font-sans bg-white text-dark antialiased transition-colors duration-300">
       <CustomCursor />
-        {/* Structure principale */}
         <div className="min-h-screen flex flex-col">
           <main className="flex-1 overflow-x-hidden">
             <div className="max-w-screen-3xl mx-auto px-4 sm:px-6 lg:px-8">
